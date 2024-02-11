bc4c8b5355de6affc7de1f5953ce179decfd24c1
Awais Afzal <129833448+awaisafzal229@users.noreply.github.com>
2024-02-11 17:59:31 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript code for handling the mobile menu dropdown
+$(document).ready(function () {
+  $(".navbar-toggle").click(function () {
+    $(".navbar-collapse").toggleClass("collapse");
+  });
+});
